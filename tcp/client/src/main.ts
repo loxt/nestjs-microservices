3c242360839ab8d8e4d0ad25f0255d3b97a48c93
loxt <emannuelloxt@hotmail.com>
2020-10-20 13:44:35 -0400

    feat: sending data using client

--- tcp/client/src/main.ts
+++ tcp/client/src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(8080);
+// }
+// bootstrap();
+
+import {
+  ClientOptions,
+  ClientProxy,
+  ClientProxyFactory,
+  Closeable,
+  Transport,
+} from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
+
+const microserviceOptions: ClientOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: 8082,
+  },
+};
+
+const client: ClientProxy & Closeable = ClientProxyFactory.create(
+  microserviceOptions,
+);
+
+client
+  .send<number, number[]>('add', [5, 2, 3])
+  .subscribe((res: number) => logger.log(res));
